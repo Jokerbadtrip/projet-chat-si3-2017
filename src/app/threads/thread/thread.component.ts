d98a1e4eb2bdcc870a1fc716e8ef4008ad1bedf3
Simon Serrano <simon.serrano@etu.unice.fr>
2017-06-12 10:26:54 +0200

    initialization of threads (channel)

---
+++ src/app/threads/thread/thread.component.ts
@@ -0,0 +1,9 @@
+
+import { Component, Input, OnInit } from "@angular/core";
+
+import { ChanelModel } from "../../../shared/models/ChannelModel";
+
+@Component({
+  selector: "app-thread",
+  templateUrl : "./thread.component"
+})
