6754a855ddfc02cb01c5722cdc9c0d2c340aeb68
Simon Serrano <simon.serrano@etu.unice.fr>
2017-06-12 17:18:10 +0200

    Channel service completed, we can add a Thread and refresh Thread;

--- src/shared/services/channel/channel.service.ts
+++ src/shared/services/channel/channel.service.ts
@@ -15,14 +15,48 @@ export class ChannelService{
    * La documentation des methodes du service permet d'avoir plus d'information concernant la façon d'accèder aux messages.
    */
   private url: string;
-
   public channelList$: ReplaySubject<ChanelModel[]>;
 
   constructor(private http: Http) {
     this.url = URLSERVER;
     this.channelList$ = new ReplaySubject(1);
     this.channelList$.next([new ChanelModel(1)]);
+  }
 
+  public createChannel(route: string, channel: ChanelModel){
+
+    console.log("createChannel:start");
+    console.log("route:" + route);
+    console.log("channel:" + channel.id);
+
+    const finalUrl = URLSERVER + route;
+    const header = new Headers({"Content-Type": "application/json"});
+    const options = new RequestOptions({headers: header});
+
+    if (route && channel) {
+      this.http.post(finalUrl, options).subscribe((response) => this.extractChannelAndGetChannel(response, route));
+      this.http.get(finalUrl).subscribe((response) => this.exctractAndUpdateChannelList(response));
+    }
+    console.log("createChannel:end");
+  }
+
+  private extractChannelAndGetChannel(response: Response, route: string): ChanelModel{
+    console.log("extractChannelAndGetChannel:start");
+    console.log("response:" + response.json());
+
+    const id = response.json().id;
+    const name = response.json().name;
+    const createdAt = response.json().createdAt;
+    const updatedAt = response.json().updatedAt;
+    const channel = new ChanelModel(id, name, createdAt, updatedAt);
+    console.log("extractChannelAndGetChannel:end");
+    return channel;
+  }
+
+  private exctractAndUpdateChannelList(response){
+    console.log("exctractAndUpdateChannelList");
+    const channelList = response.json() || [];
+    this.channelList$.next(channelList);
   }
 
 }
